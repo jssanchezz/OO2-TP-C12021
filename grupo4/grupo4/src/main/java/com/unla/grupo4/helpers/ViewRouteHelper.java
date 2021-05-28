1a424e27453570c17cb2022088d67b72af5ff881
jssanchezz <jssanchezz20@gmail.com>
2021-05-28 18:28:08 -0300

    Casos de uso 1, 2 y 3
    Se agregaron los casos de uso:
    1. ABM Perfiles/Roles
    2. Listar los perfiles
    3. Descargar los perfiles en pdf
    
    Anteriormente habia creado una branch por cada cu, pero por un error tuve que volver a subirlas.

--- grupo4/grupo4/src/main/java/com/unla/grupo4/helpers/ViewRouteHelper.java
+++ grupo4/grupo4/src/main/java/com/unla/grupo4/helpers/ViewRouteHelper.java
@@ -2,17 +2,31 @@ package com.unla.grupo4.helpers;
 
 public class ViewRouteHelper {
 	
-	//Views
-	public final static String USER_INDEX = "user/index";
-	public final static String USER_FORM = "user/form";
-	public final static String USER_UPDATE = "user/update";
-	public final static String USER_UPDATE_FORM = "user/formUpdate";
-	public final static String USER_DELETE = "user/delete";
-	public final static String USER_LIST = "user/listUsers";
+	//User
+		//Views
+		public final static String USER_INDEX = "user/index";
+		public final static String USER_FORM = "user/form";
+		public final static String USER_UPDATE = "user/update";
+		public final static String USER_UPDATE_FORM = "user/formUpdate";
+		public final static String USER_DELETE = "user/delete";
+		public final static String USER_LIST = "user/listUsers";
+		
+		//Redirect
+		public final static String ROUTE_USER_ROOT = "/index";
+		public final static String ROUTE_USER_FORM = "/users/newUser";
+		public final static String ROUTE_USER_UPDATE = "/users/update";
+		public final static String ROUTE_USER_DELETE = "/users/deleteUser";
 	
-	//Redirect
-	public final static String ROUTE_USER_ROOT = "/index";
-	public final static String ROUTE_USER_FORM = "/users/newUser";
-	public final static String ROUTE_USER_UPDATE = "/users/update";
-	public final static String ROUTE_USER_DELETE = "/users/deleteUser";
+	//UserRole
+		//Views
+		public final static String USER_ROLE_INDEX = "userRole/index";
+		public final static String USER_ROLE_INSERT = "userRole/form";		
+		public final static String USER_ROLE_DELETE = "userRole/delete";
+		public final static String USER_ROLE_UPDATE = "userRole/update";
+		public final static String USER_ROLE_LIST = "userRole/list";
+			
+		//Redirects
+		public final static String USER_ROLE_ROOT = "/userRoles/";
+		public final static String USER_ROLE_DELETE_ROOT = "/userRoles/deleteRole";
+		public final static String USER_ROLE_INSERT_ROOT = "/userRoles/newRole";
 }
---
+++ grupo4/grupo4/src/main/java/com/unla/grupo4/miscelaneo/UserRolePDFExporter.java
@@ -0,0 +1,79 @@
+package com.unla.grupo4.miscelaneo;
+
+import java.awt.Color;
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.Font;
+import com.lowagie.text.FontFactory;
+import com.lowagie.text.PageSize;
+import com.lowagie.text.Paragraph;
+import com.lowagie.text.Phrase;
+import com.lowagie.text.pdf.PdfPCell;
+import com.lowagie.text.pdf.PdfPTable;
+import com.lowagie.text.pdf.PdfWriter;
+import com.unla.grupo4.entities.UserRole;
+
+public class UserRolePDFExporter {
+	
+    private List<UserRole> listUserRoles;
+     
+    public UserRolePDFExporter(List<UserRole> listUserRoles) {
+        this.listUserRoles = listUserRoles;
+    }
+ 
+    private void writeTableHeader(PdfPTable table) {
+        PdfPCell cell = new PdfPCell();
+        cell.setBackgroundColor(Color.ORANGE);
+        cell.setPadding(5);
+         
+        Font font = FontFactory.getFont(FontFactory.HELVETICA);
+        font.setColor(Color.DARK_GRAY);
+         
+        cell.setPhrase(new Phrase("Perfil ID", font));
+         
+        table.addCell(cell);
+         
+        cell.setPhrase(new Phrase("Nombre", font));
+        table.addCell(cell);   
+    }
+     
+    private void writeTableData(PdfPTable table) {
+        for (UserRole ur : this.listUserRoles) {
+			table.addCell(String.valueOf(ur.getId()));
+            table.addCell(ur.getRole());            
+        }
+    }
+     
+    public void export(HttpServletResponse response) throws DocumentException, IOException {
+        Document document = new Document(PageSize.A4);
+        PdfWriter.getInstance(document, response.getOutputStream());
+         
+        document.open();
+        Font font = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
+        font.setSize(18);
+        font.setColor(Color.LIGHT_GRAY);
+         
+        Paragraph p = new Paragraph("Lista de perfiles", font);
+        p.setAlignment(Paragraph.ALIGN_CENTER);
+         
+        document.add(p);
+         
+        PdfPTable table = new PdfPTable(2);
+        table.setWidthPercentage(100f);
+        table.setWidths(new float[] {1.5f, 3.5f});
+        table.setSpacingBefore(10);
+         
+        writeTableHeader(table);
+        writeTableData(table);
+         
+        document.add(table);
+         
+        document.close();
+         
+    }
+}
