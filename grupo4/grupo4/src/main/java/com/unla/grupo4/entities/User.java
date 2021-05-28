886d91442d4ceee70e31db57121763430cbfc32c
federico-procs <71523856+federico-procs@users.noreply.github.com>
2021-05-28 20:11:35 -0300

    Login 2.1

--- grupo4/grupo4/src/main/java/com/unla/grupo4/entities/User.java
+++ grupo4/grupo4/src/main/java/com/unla/grupo4/entities/User.java
@@ -11,8 +11,6 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
-
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
