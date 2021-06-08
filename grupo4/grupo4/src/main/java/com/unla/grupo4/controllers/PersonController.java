b5c9d23d699e857dd3aae9f6c2ba6b56e8bf5832
jssanchezz <jssanchezz20@gmail.com>
2021-06-07 21:03:51 -0300

    Personas Controller
    Se agrego validacion para que no se agregue personas con el mismo DNI.
    Se agrego logica para mensajes a traves de RedirectAttributes.

--- grupo4/grupo4/src/main/java/com/unla/grupo4/controllers/PersonController.java
+++ grupo4/grupo4/src/main/java/com/unla/grupo4/controllers/PersonController.java
@@ -8,11 +8,13 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
 
 import com.unla.grupo4.helpers.ViewRouteHelper;
 import com.unla.grupo4.models.PersonModel;
 import com.unla.grupo4.services.IPersonService;
+import com.unla.grupo4.services.IUserService;
 
 @Controller
 @RequestMapping("/persons")
@@ -21,17 +23,31 @@ public class PersonController {
 	@Autowired
 	@Qualifier("personService")
 	private IPersonService personService;
+	
+	@Autowired
+	@Qualifier("userService")
+	private IUserService userService;
 
 	@GetMapping("/newPerson")
 	public ModelAndView form() {
 		ModelAndView mAV = new ModelAndView(ViewRouteHelper.PERSON_FORM);
 		mAV.addObject("person", new PersonModel());
+		
+		mAV.addObject("userlogrole", userService.getRoleOfUserLog());
 		return mAV;
 	}
 	
 	@PostMapping("/createPerson")
-	public RedirectView createPerson(@ModelAttribute("person") PersonModel personModel) {
-		personService.insertOrUpdate(personModel);
+	public RedirectView createPerson(@ModelAttribute("person") PersonModel personModel, RedirectAttributes attribute) {
+		if(personService.findByDni(personModel.getDni()) == null) {
+			personService.insertOrUpdate(personModel);
+			attribute.addFlashAttribute("mensaje", "Guardado correctamente");
+	        attribute.addFlashAttribute("clase", "success");
+		}else {
+			attribute.addFlashAttribute("mensaje", "Persona ya existente con dni: " + personModel.getDni());
+	        attribute.addFlashAttribute("clase", "warning");
+		}
+		
 		return new RedirectView(ViewRouteHelper.ROUTE_PERSON_FORM);
 	}
 
