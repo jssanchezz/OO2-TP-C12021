176400c7e8aa1785aa5c9ae26c61eef3d6c15266
jssanchezz <jssanchezz20@gmail.com>
2021-06-13 12:45:57 -0300

    Modificacion Script y delete de archivos no utilizables

--- grupo4/grupo4/src/main/java/com/unla/grupo4/configuration/SecurityConfig.java
+++ grupo4/grupo4/src/main/java/com/unla/grupo4/configuration/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/css/**", "/js/**", "/img/*","/vendor/**","/eastereggs/*",
+		http.authorizeRequests().antMatchers("/css/**", "/js/**", "/img/*","/vendor/**",
 				"/rodado/*","/persons/*","/permiso/**","/*").permitAll()
 		.anyRequest().authenticated()
 		.and()
